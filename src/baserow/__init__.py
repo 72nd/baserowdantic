76f4f4acdedcecbdea670d45022547a3227cdae0
alex-berlin-tv <hostettler@alex-berlin.de>
2024-07-16 20:12:16 +0200

    pdoc options are now only set if pdoc is installed

--- src/baserow/__init__.py
+++ src/baserow/__init__.py
@@ -3,6 +3,11 @@
    :start-after: baserowdantic
 """
 
-from pdoc import render_helpers
+import atexit
+from importlib import util as importlib_util
 
-render_helpers.markdown_extensions["toc"]["depth"] = 3
+# Tweaks the rendering of the documentation's table of contents to show three
+# levels deep.
+if importlib_util.find_spec("pdoc") is not None:
+    from pdoc import render_helpers
+    render_helpers.markdown_extensions["toc"]["depth"] = 3
